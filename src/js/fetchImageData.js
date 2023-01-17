34da035e31699b9dbf4a107e0eff26c5ee654bd4
HannaMois <hannamois135@gmail.com>
2023-01-17 19:32:45 +0200

    Update js

--- src/js/fetchImageData.js
+++ src/js/fetchImageData.js
@@ -3,14 +3,12 @@ import axios from 'axios';
 export default async function fetchImage(value, page) {
     const BASE_URL = 'https://pixabay.com/api/';
     const API_KEY = '32841517-26afb12f8bbb7df3fa6709fac';
-    const searchParams = {
-        params: {
+    const searchParams = new URLSearchParams({ 
         image_type:"photo",
         orientation:"horizontal",
         safesearch:"true",
         per_page: 40,
-        },
-    };
+    });
     
     const filter = `?key=${API_KEY}&q=${value}&${searchParams}&page=${page}`;
     return await axios.get(`${BASE_URL}${filter}`).then(response => response.data);
